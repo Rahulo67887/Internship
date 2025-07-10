9ef64e2cf0822c2664d79556beab91caea96f535
Rahul <rc7662837@gmail.com>
2025-07-10 16:24:51 +0545

    created a spinner and defined its css and animation

---
+++ Frontend/src/components/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+const LoadingSpinner : React.FC =()=>{
+    return(
+        <div className="loading-spinner">
+            <div className="spinner"></div>
+            <p>Analyzing your reflection...</p>
+        </div>
+    )
+};
+
+export default LoadingSpinner;
