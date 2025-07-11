108421c6b83a252ad29c6dc64b756829adbb5fcb
Rahul <rc7662837@gmail.com>
2025-07-11 16:43:45 +0545

    corrected the basse api url in store

--- Frontend/src/store/EmotionStore.tsx
+++ Frontend/src/store/EmotionStore.tsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, type ReactNode} from "react";
 
-const API_BASE_URL = 'http://127.0.0.1:800';
+const API_BASE_URL = 'http://127.0.0.1:8000';
 
 
 interface EmotionResult{
